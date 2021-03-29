c49054d8783c05383f3cdf7f11761490bbdd2c5e
drug007 <drug2004@bk.ru>
2021-03-29 22:26:25 +0300

    Revert me please

--- test/run-unittest.d
+++ test/run-unittest.d
@@ -106,14 +106,14 @@ int main(string[] args)
 			auto logFile = File("log.log", "w"); // dummy
 			if (buildPath(pack.name, ".fail_build").exists)
 			{
-				log("Building ", pack.name, ", expected failure...");
+				log("Building ", pack.name, "/, expected failure...");
 				// $DUB build --force --root=$pack --compiler=$DC 2>/dev/null && logError "Error: Failure expected, but build passed."
 				if (!spawnProcess([dub, "build", "--force", "--root=" ~ pack.name, "--compiler=" ~ dc], stdin, logFile).wait)
 					logError("Error: Failure expected, but build passed.");
 			}
 			else
 			{
-				log("Building ", pack.name, "...");
+				log("Building ", pack.name, "/...");
 				// $DUB build --force --root=$pack --compiler=$DC || logError "Build failure."
 				if (spawnProcess([dub, "build", "--force", "--root=" ~ pack.name, "--compiler=" ~ dc], stdin, logFile).wait)
 					logError("Build failure");
@@ -126,7 +126,7 @@ int main(string[] args)
 		// if [ $build -eq 1 ] && [ ! -e $pack/.no_run ] && [ ! -e $pack/.no_run_$DC_BIN ]; then
 		if (build && !buildPath(pack.name, ".no_run").exists && !buildPath(pack.name, ".no_run_" ~ dc_bin).exists)
 		{
-			log("Running ", pack.name, "...");
+			log("Running ", pack.name, "/...");
 		// 	# $DUB run --force --root=$pack --compiler=$DC || logError "Run failure."
 			if (spawnProcess([dub, "run", "--force", "--root=" ~ pack.name, "--compiler=" ~ dc]).wait)
 				logError("Run failure");
@@ -136,7 +136,7 @@ int main(string[] args)
 		// if [ $build -eq 1 ] && [ ! -e $pack/.no_test ] && [ ! -e $pack/.no_test_$DC_BIN ]; then
 		if (build && !buildPath(pack.name, ".no_test").exists && !buildPath(pack.name, ".no_test_" ~ dc_bin).exists)
 		{
-			log("Testing ", pack.name, "...");
+			log("Testing ", pack.name, "/...");
 		// 	# $DUB test --force --root=$pack --compiler=$DC || logError "Test failure."
 			if (spawnProcess([dub, "test", "--force", "--root=" ~ pack.name, "--compiler=" ~ dc]).wait)
 				logError("Test failure");
