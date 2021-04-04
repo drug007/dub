8295e8e671f6776b980deeda730689384de29e2f
drug007 <drug2004@bk.ru>
2021-04-04 18:12:14 +0300

    wip

--- diff.d
+++ diff.d
@@ -4,10 +4,9 @@
 	targetName: diff
 +/
 
-int main()
+void main()
 {
 	import std;
-getcwd.writeln;
 	auto l1 = "test/test.log".readText.lineSplitter.array.sort;
 	auto l2 = "test/test2.log".readText.lineSplitter.array.sort;
 
@@ -16,5 +15,12 @@ getcwd.writeln;
 	writeln("diff");
 	string[] d = setSymmetricDifference(l1, l2).array;
 	d.each!writeln;
-	return d.length == 0 ? 0 : 1;
+	assert(d.equal([
+		"[ERROR] /home/runner/work/dub/dub/test/version-spec.sh:19 command failed",
+		"[ERROR] /home/runner/work/dub/dub/test/version-spec.sh:20 command failed",
+		"[ERROR] /home/runner/work/dub/dub/test/version-spec.sh:21 command failed",
+		"[ERROR] /opt/hostedtoolcache/dc/dmd-2.096.0/x64/dmd2/linux/bin64/dub:19 command failed",
+		"[ERROR] /opt/hostedtoolcache/dc/dmd-2.096.0/x64/dmd2/linux/bin64/dub:20 command failed",
+		"[ERROR] /opt/hostedtoolcache/dc/dmd-2.096.0/x64/dmd2/linux/bin64/dub:21 command failed",
+	]));
 }
