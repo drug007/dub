d44bf3d1921bc3483cb15790aaeba521de9937a7
drug007 <drug2004@bk.ru>
2021-04-05 00:29:50 +0300

    wip

--- linux_diff.d
+++ linux_diff.d
@@ -12,14 +12,8 @@ void main()
 
 	string[] d = setSymmetricDifference(l1, l2).array;
 	d.each!writeln;
-	version(Posix) assert(d.equal([
-		"[ERROR] /home/runner/work/dub/dub/test/version-spec.sh:19 command failed",
-		"[ERROR] /home/runner/work/dub/dub/test/version-spec.sh:20 command failed",
-		"[ERROR] /home/runner/work/dub/dub/test/version-spec.sh:21 command failed",
-		"[ERROR] /opt/hostedtoolcache/dc/dmd-2.096.0/x64/dmd2/linux/bin64/dub:19 command failed",
-		"[ERROR] /opt/hostedtoolcache/dc/dmd-2.096.0/x64/dmd2/linux/bin64/dub:20 command failed",
-		"[ERROR] /opt/hostedtoolcache/dc/dmd-2.096.0/x64/dmd2/linux/bin64/dub:21 command failed",
-	]));
+	version(Posix)
+		assert(d.length == 0);
 	else
 		static assert(0);
 }
