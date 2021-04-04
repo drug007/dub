c6ee11ead58cfb95cce5e74d9b30d6aa688ecdb1
drug007 <drug2004@bk.ru>
2021-04-04 14:55:46 +0300

    set log file name from environment

--- test/common.sh
+++ test/common.sh
@@ -2,9 +2,11 @@ SOURCE_FILE=$_
 
 set -ueEo pipefail
 
+FILENAME="${LOG_FILE:-test.log}"
+
 function log() {
     echo -e "\033[0;33m[INFO] $@\033[0m"
-    echo "[INFO] $@" >> $(dirname "${BASH_SOURCE[0]}")/test.log
+    echo "[INFO] $@" >> $(dirname "${BASH_SOURCE[0]}")/"${FILENAME}"
 }
 
 # lineno[, msg]
@@ -12,7 +14,7 @@ function die() {
     local line=$1
     local msg=${2:-command failed}
     local supplemental=${3:-}
-    echo "[ERROR] $SOURCE_FILE:$1 $msg" | tee -a $(dirname "${BASH_SOURCE[0]}")/test.log | cat 1>&2
+    echo "[ERROR] $SOURCE_FILE:$1 $msg" | tee -a $(dirname "${BASH_SOURCE[0]}")/"${FILENAME}" | cat 1>&2
     if [ ! -z "$supplemental" ]; then
         echo "$supplemental" | >&2 sed 's|^|        |g'
     fi
