802b94ecf70732d9ef604bf9a349bad29ed30054
drug007 <drug2004@bk.ru>
2021-04-04 16:19:04 +0300

    wip

--- diff.d
+++ diff.d
@@ -7,9 +7,9 @@
 int main()
 {
 	import std;
-
-	auto l1 = "test.log".readText.lineSplitter.array.sort;
-	auto l2 = "test2.log".readText.lineSplitter.array.sort;
+getcwd.writeln;
+	auto l1 = "test/test.log".readText.lineSplitter.array.sort;
+	auto l2 = "test/test2.log".readText.lineSplitter.array.sort;
 
 	auto i = setIntersection(l1, l2);
 	i.each!writeln;
